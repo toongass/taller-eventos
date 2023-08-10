25b0d9c9642143113534ce90fc6c2969ad810ad0
toongass <119079321+toongass@users.noreply.github.com>
2023-08-10 14:38:13 -0300

    solucion-click
    se elimina el onlclick del html y se crea un evento para el boton en eventos.js usando stopPropagation() que evita que salgan los dos eventos juntos dando click al boton

--- eventos.js
+++ eventos.js
@@ -1,4 +1,13 @@
-const div = document.querySelector("div");
+const button = document.getElementById("boton");
+button.addEventListener("click", function(evento){
+    alert("hola!");
+    evento.stopPropagation();
+
+});
+
+
+
+const div = document.getElementById("myDiv");
 div.addEventListener("click", function(){
     alert("Hola! soy el div")
 });
